ed7f8ab155eaea8c7f55bdc941b11bd1ddde74a4
yonyossef <yonyossef3@gmail.com>
2021-11-15 18:31:07 +0200

    added a link to sendgrids sender verification

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
+  # this address is a SendGrid's verified sender (https://app.sendgrid.com/settings/sender_auth/senders)
   default from: 'test@cashapp.co.il'
   layout 'mailer'
 end
